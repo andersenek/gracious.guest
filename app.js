e0344f5861edf1630dd7741ad3d90ea3d7bf10dd
Erica Andersen <andersenek@gmail.com>
2015-09-14 19:27:48 -0400

    Added comment to event functionality

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.config([
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+      })
+
+      .state('events', {
+        url: '/events/{id}',
+        templateUrl: '/events.html',
+        controller: 'EventsCtrl'
       });
 
     $urlRouterProvider.otherwise('home'); // Redirect to home
@@ -36,18 +42,45 @@ app.controller('MainCtrl', [
     $scope.events = events.events; // Bind the $scope.events variable in our controller to the events array in our service
 
     $scope.addEvent = function(){ // Allow user to add a event
-      if(!$scope.title || $scope.title === '') {  // Prevent the user from eventing a blank title
+      if(!$scope.title || $scope.title === '') {  // Prevent the user from entering a blank title
         return;
       }
 
       $scope.events.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        comments: [
+          {author: 'Erica', body: 'This is so cool'},
+          {author: 'Kanye', body: 'Woah this is awwwwesome'}
+        ]
       });
 
-      $scope.title = '';
-      $scope.link = '';
+      $scope.title = ''; // Set area to empty when done
+      $scope.link = ''; // Set area to empty when done
     };
 
   }]); // End MainCtrl controller
+
+app.controller('EventsCtrl', [
+  '$scope',
+  '$stateParams',
+  'events', // Need access events so we can retreive the ID
+
+  function($scope, $stateParams, events){
+
+    $scope.event = events.events[$stateParams.id];
+
+    $scope.addComment = function(){
+      if($scope.body === '') { // Prevent the user from entering a blank body
+        return;
+       }
+
+      $scope.event.comments.push({ // Comment will post to an event
+        body: $scope.body,
+        author: 'user'
+      });
+
+      $scope.body = ''; // Set area to empty when done
+    };
+
+}]); // End EventsCtrl controller
